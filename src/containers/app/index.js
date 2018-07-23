b5c54f900ace6a464fd0473d9ca140ac7a9ef30c
Xinlan Emily Hu <xehu@stanford.edu>
2018-07-23 16:37:50 -0700

    set up basic chatbot, but has some bugs (see comment)

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,18 +2,51 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import ChatBot from 'react-simple-chatbot'
+import EndCallback from '.'
+
+// var EndCallback = 
+// EndCallback.render()
+
+const steps = [
+  {
+    id: '0',
+    message: 'Hi, I\'m Oneboarding! I\'m here to help you onboard.',
+
+    //Below is code trying to create a sample dialogue. Known bug is that it won't let you type.
+
+    //trigger: '1',
+  },
+  /*{
+    id: '1',
+    message: 'To get started, go to aka.ms/internz.',
+    trigger: '2',
+  },
+  {
+    id: '2',
+    message: 'Let me know when you\'re done reading.',
+  },
+  {
+    id: '3',
+    user: true,
+    validator: (value) => {
+        if (value === "got it" || value === "done") {
+          return true;
+        }
+        return "Oh no, how can I help?";
+    },
+    trigger:'4',
+  },
+  {
+    id: '4',
+    message: 'Can I answer a question?',
+  }*/
+];
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
-    </header>
-
-    <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
-    </main>
+    /*<EndCallback />*/
+    <ChatBot steps={steps} />
   </div>
 )
 
